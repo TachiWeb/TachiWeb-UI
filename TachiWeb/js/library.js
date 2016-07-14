e972dc6515adad4416c063bc0a441df6721c4a7f
nulldev <contactnulldev@gmail.com>
2016-07-14 17:36:29 +0000

    Add very basic category tabs.

--- TachiWeb/js/library.js
+++ TachiWeb/js/library.js
@@ -1,72 +1,149 @@
-//TODO We have literally no error handling, maybe a snackbar or something when an error occurs?
 var coverRoot = apiRoot + "/cover";
 var libraryRoot = apiRoot + "/library";
 var librarySpinner;
-var libraryGrid;
+var libraryWrapper;
+var libraryTabs;
 
 function updateLibrary() {
-	showSpinner();
+    showSpinner();
     var xhr = new XMLHttpRequest();
     xhr.open("GET", libraryRoot, true);
-    xhr.onload = function () {
-		try {
-        	var parsed = JSON.parse(xhr.responseText);
-        	updateLibraryUI(parsed);
-		} catch(e) {
-			libraryUpdateError();
-		}
-		hideSpinner();
+    xhr.onload = function() {
+        try {
+            var parsed = JSON.parse(xhr.responseText);
+            updateLibraryUI(parsed);
+        }
+        catch (e) {
+            libraryUpdateError();
+        }
+        hideSpinner();
     };
-	xhr.onerror = function() {
-		libraryUpdateError();
-		hideSpinner();
-	};
+    xhr.onerror = function() {
+        libraryUpdateError();
+        hideSpinner();
+    };
     xhr.send();
 }
 
 function showSpinner() {
-	librarySpinner.css("opacity", 1);
+    librarySpinner.css("opacity", 1);
 }
+
 function hideSpinner() {
-	librarySpinner.css("opacity", 0);
+    librarySpinner.css("opacity", 0);
 }
 
 function buildCoverUrl(mangaId) {
     return coverRoot + "/" + mangaId;
 }
 
+function showTabs() {
+
+}
+
+function hideTabs() {
+
+}
+
 function updateLibraryUI(mangas) {
-    clearElement(libraryGrid[0]);
+    //Construct categories
+    var categories = {};
     for (var i = 0; i < mangas.length; i++) {
         var manga = mangas[i];
-        var card = document.createElement("div");
-        card.className = "mdl-card mdl-shadow--4dp manga_card mdl-cell mdl-cell--2-col mdl-button mdl-js-button mdl-js-ripple-effect";
-        var img = document.createElement("img");
-        img.className = "manga_card_img";
-        img.src = buildCoverUrl(manga.id);
-        img.alt = manga.title;
-        card.appendChild(img);
-        var label = document.createElement("div");
-        label.className = "manga_card_label";
-        label.textContent = manga.title;
-        card.appendChild(label);
-        libraryGrid[0].appendChild(card);
-		//Make sure MDL gets these changes
-        componentHandler.upgradeElement(card);
+        for (var a = 0; a < manga.categories.length; a++) {
+            var categoryName = manga.categories[a];
+            var category = categories[categoryName];
+            if (!valid(category)) {
+                category = [];
+                categories[categoryName] = category;
+            }
+            category.push(manga);
+        }
+    }
+    //Remove old entries
+    clearElement(libraryWrapper[0]);
+    clearElement(libraryTabs[0]);
+    var categoryKeys = Object.keys(categories);
+    if (categoryKeys.length <= 1) {
+        //Append directly if we don't have tabs
+        appendMangas(mangas, libraryWrapper[0]);
+    } else {
+        for(i = 0; i < categoryKeys.length; i++) {
+            categoryName = categoryKeys[i];
+            category = categories[categoryName];
+            var categoryId = "category-tab-" + i;
+            //Generate tabs
+            var categoryTab = document.createElement("a");
+            categoryTab.className = "mdl-layout__tab";
+            //First tab is active
+            if(i === 0) {
+                categoryTab.className += " is-active";
+            }
+            categoryTab.href = "#" + categoryId;
+            categoryTab.textContent = categoryName;
+            libraryTabs[0].appendChild(categoryTab);
+            //Generate content
+            var categoryPanel = document.createElement("div");
+            categoryPanel.className = "mdl-tabs__panel";
+            //First panel is active
+            if(i === 0) {
+                categoryPanel.className += " is-active";
+            }
+            categoryPanel.id = categoryId;
+            libraryWrapper[0].appendChild(categoryPanel);
+            //Actually append mangas
+            appendMangas(category, categoryPanel);
+        }
+        //Make sure MDL gets tab changes
+        componentHandler.upgradeElement(libraryTabs[0]);
+    }
+    //Make sure MDL gets content changes
+    componentHandler.upgradeElement(libraryWrapper[0]);
+}
+
+function appendMangas(mangas, rootElement) {
+    var libraryGrid = createGrid();
+    rootElement.appendChild(libraryGrid);
+    for (var i = 0; i < mangas.length; i++) {
+        appendManga(mangas[i], libraryGrid);
     }
 }
-function libraryUpdateError() {
-	snackbar.showSnackbar({
-		message: "Error getting library!",
-		timeout: 2000,
-		actionText: "Retry",
-		actionHandler: function() {
-			updateLibrary();
-		}
-	});
+
+function createGrid() {
+    var grid = document.createElement("div");
+    grid.className = "mdl-grid";
+    return grid;
 }
+
+function appendManga(manga, element) {
+    var card = document.createElement("div");
+    card.className = "mdl-card mdl-shadow--4dp manga_card mdl-cell mdl-cell--2-col mdl-button mdl-js-button mdl-js-ripple-effect";
+    var img = document.createElement("img");
+    img.className = "manga_card_img";
+    img.src = buildCoverUrl(manga.id);
+    img.alt = manga.title;
+    card.appendChild(img);
+    var label = document.createElement("div");
+    label.className = "manga_card_label";
+    label.textContent = manga.title;
+    card.appendChild(label);
+    element.appendChild(card);
+}
+
+function libraryUpdateError() {
+    snackbar.showSnackbar({
+        message: "Error getting library!",
+        timeout: 2000,
+        actionText: "Retry",
+        actionHandler: function() {
+            updateLibrary();
+        }
+    });
+}
+
 function onLoad() {
-    libraryGrid = $("#manga_grid");
-	librarySpinner = $(".loading_spinner");
+    librarySpinner = $(".loading_spinner");
+    libraryWrapper = $("#library_wrapper");
+    libraryTabs = $("#library_tabs");
     updateLibrary();
 }
