770e0d8754f37239fbc549d932997cf61212ee5d
nulldev <contactnulldev@gmail.com>
2016-07-19 19:12:58 -0400

    The navigation drawer is now populated in JS.

---
+++ TachiWeb/js/drawer.js
@@ -0,0 +1,26 @@
+/**
+ Global drawer
+ **/
+var drawer;
+var drawerTitle;
+var drawerNav;
+
+function setupDrawer() {
+    drawerTitle.text("TachiWeb");
+    insertDrawerLink("Library", "index.html");
+}
+
+function insertDrawerLink(text, link) {
+    var drawerElementLink = document.createElement("a");
+    drawerElementLink.className = "mdl-navigation__link";
+    drawerElementLink.href = link;
+    drawerElementLink.textContent = text;
+    rawElement(drawerNav).appendChild(drawerElementLink);
+}
+
+$(document).ready(function(){
+    drawer = $(".mdl-layout__drawer");
+    drawerTitle = drawer.find(".mdl-layout-title");
+    drawerNav = drawer.find(".mdl-navigation");
+    setupDrawer();
+});
