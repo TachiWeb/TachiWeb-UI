d69a99b0978d1735f9e604e214bad56930ef6409
nulldev <contactnulldev@gmail.com>
2016-07-23 18:47:40 -0400

    Switch to debug API.

--- js/core.js
+++ js/core.js
@@ -1,4 +1,5 @@
-var apiRoot = "/api";
+// var apiRoot = "/api";
+var apiRoot = "http://localhost:4567/api";
 var coverRoot = apiRoot + "/cover";
 var libraryRoot = apiRoot + "/library";
 var infoRoot = apiRoot + "/manga_info";
