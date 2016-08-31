ac956a2dceb115fbd75d9114bd8436cef9210fb1
nulldev <contactnulldev@gmail.com>
2016-08-31 12:11:16 -0400

    Removed unnecessary code in options_api.

--- js/options_api.js
+++ js/options_api.js
@@ -3,7 +3,6 @@ var OptionsApi = {};
 (function () {
     TWApi.Commands.GetPrefs.execute(function (resp) {
         var options = resp.prefs;
-        OptionsApi.all_options = options;
         var keys = Object.keys(options);
         for (var i = 0; i < keys.length; i++) {
             var key = keys[i];
