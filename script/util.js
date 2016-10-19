8d5dad6c7711932964296cb9865b04952f99b26c
nulldev <contactnulldev@gmail.com>
2016-10-19 21:03:45 +0000

    Add search UI. Code cleanup. Begin toolbar implementations.

--- script/util.js
+++ script/util.js
@@ -1,21 +1,34 @@
-function debounce(func, wait, immediate) {
-	let timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
+var Util = {
+	debounce: function(func, wait, immediate) {
+		let timeout;
+		return function() {
+			var context = this,
+				args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
 		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+	},
 
-function guid() {
-	let S4 = function() {
-		return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-	};
-	return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4();
-};
+	guid: function() {
+		let S4 = function() {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		};
+		return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4();
+	},
+
+	indexInParent: function(node) {
+		var i = 0;
+		while (node = node.previousSibling) {
+			if (node.nodeType === 1) {
+				++i
+			}
+		}
+		return i;
+	}
+}
