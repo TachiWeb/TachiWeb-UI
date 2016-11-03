4534697dae27d0b2b52aa93c5a8b6f9f90fda342
nulldev <contactnulldev@gmail.com>
2016-11-03 18:06:23 +0000

    Change settings display mode to breadcrumb. Add some simple error handling. Finish settings implementation.

--- script/util.js
+++ script/util.js
@@ -1,59 +1,69 @@
 var Util = {
-	debounce: function(func, wait, immediate) {
-		let timeout;
-		return function() {
-			var context = this,
-				args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
+		debounce: function(func, wait, immediate) {
+			let timeout;
+			return function() {
+				var context = this,
+					args = arguments;
+				var later = function() {
+					timeout = null;
+					if (!immediate) func.apply(context, args);
+				};
+				var callNow = immediate && !timeout;
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+				if (callNow) func.apply(context, args);
 			};
-			var callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
-	},
-	
-	group: function(func, wait) {
-		var calls = []; //Queued calls
-		let timeout;
-		return function() {
-			var context = this,
-				args = arguments;
-			var later = function() {
-				timeout = null;
-				//Apply queued calls
-				for(let call of calls) {
-					func.apply(context, call);
+		},
+
+		group: function(func, wait) {
+			var calls = []; //Queued calls
+			let timeout;
+			return function() {
+				var context = this,
+					args = arguments;
+				var later = function() {
+					timeout = null;
+					//Apply queued calls
+					for (let call of calls) {
+						func.apply(context, call);
+					}
+					calls = [];
+				};
+				//Queue the call
+				calls.push(args);
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+			};
+		},
+
+		guid: function() {
+			let S4 = function() {
+				return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+			};
+			return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4();
+		},
+
+		indexInParent: function(node) {
+			var i = 0;
+			while (node = node.previousSibling) {
+				if (node.nodeType === 1) {
+					++i
 				}
-				calls = [];
-			};
-			//Queue the call
-			calls.push(args);
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-		};
-	},
-
-	guid: function() {
-		let S4 = function() {
-			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-		};
-		return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4();
-	},
-
-	indexInParent: function(node) {
-		var i = 0;
-		while (node = node.previousSibling) {
-			if (node.nodeType === 1) {
-				++i
 			}
+			return i;
+		},
+
+		isOffscreen: function(el) {
+			var rect = el.getBoundingClientRect();
+			return (
+				rect.left < 0 ||
+				rect.top < 0 ||
+				rect.right > window.innerWidth ||
+				rect.bottom > window.innerHeight
+			);
 		}
-		return i;
 	}
-}
-//Utility array peek function
+	//Utility array peek function
 Array.prototype.peek = function() {
 	return this[this.length - 1];
 }
